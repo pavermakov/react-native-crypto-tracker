c9e868a9c66ce6729a668933a0474b1adcc9ea63
Pavel Ermakov <Pavel1994-09@mail.ru>
2018-04-13 01:28:42 +0300

    MOBILE-3. Displayed result on a screen

---
+++ src/components/CoinCard.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+import images from '../utils/coinIcons';
+
+const CoinCard = ({ symbol, coin_name, price_usd, percent_change_24h, percent_change_7d }) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.upperRow}>
+        <Image
+          style={styles.image}
+          source={{ uri: images[symbol] }}
+        />
+        <Text style={styles.coinSymbol}>{symbol}</Text>
+        <Text style={styles.seperator}>|</Text>
+        <Text style={styles.coinName}>{coin_name}</Text>
+
+        <Text style={styles.coinPrice}>{price_usd}
+          <Text style={styles.moneySymbol}> $ </Text>
+        </Text>
+      </View>
+
+      <View style={styles.statisticsContainer}>
+        <Text>24h:
+          <Text style={styles.percent_change_24h < 0 ? styles.percentChangeMinus : styles.percentChangePlus }> {percent_change_24h} % </Text>
+        </Text>
+
+        <Text>7d:
+          <Text style={styles.percent_change_7d < 0 ? styles.percentChangeMinus : styles.percentChangePlus }> {percent_change_7d} % </Text>
+        </Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    marginBottom: 20,
+    borderBottomColor: "#e5e5e5",
+    borderBottomWidth: 3,
+    padding: 20
+  },
+  upperRow: {
+    display: "flex",
+    flexDirection: "row",
+    marginBottom: 15
+  },
+  coinSymbol: {
+    marginTop: 10,
+    marginLeft: 20,
+    marginRight: 5,
+    fontWeight: "bold",
+  },
+  coinName: {
+    marginTop: 10,
+    marginLeft: 5,
+    marginRight: 20
+  },
+  seperator: {
+    marginTop: 10,
+  },
+  coinPrice: {
+    marginTop: 10,
+    marginLeft: "auto",
+    marginRight: 10,
+    fontWeight: "bold",
+  },
+  image: {
+    width: 35,
+    height: 35,
+  },
+  moneySymbol: {
+    fontWeight: "bold",
+  },
+  statisticsContainer: {
+    display: "flex",
+    borderTopColor: "#FAFAFA",
+    borderTopWidth: 2,
+    padding: 10,
+    flexDirection: "row",
+    justifyContent: "space-around"
+  },
+  percentChangePlus: {
+    color: "#00BFA5",
+    fontWeight: "bold",
+    marginLeft: 5
+  },
+  percentChangeMinus: {
+    color: "#DD2C00",
+    fontWeight: "bold",
+    marginLeft: 5
+  },
+});
+
+export default CoinCard;
--- src/containers/CryptoContainer.js
+++ src/containers/CryptoContainer.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { View, Text } from 'react-native';
+import { View, ScrollView, Text, StyleSheet } from 'react-native';
+import Spinner from 'react-native-loading-spinner-overlay';
+
+import { CoinCard } from '../components';
 import { fetchCoinData } from '../actions';
 
 class CryptoContainer extends Component {
@@ -8,14 +11,53 @@ class CryptoContainer extends Component {
     this.props.fetchCoinData();
   }
 
+  renderCoinCards = () => {
+    const { crypto } = this.props;
+
+    return crypto.data.map((coin, index) => {
+      return (
+        <CoinCard
+          key={index}
+          coin_name={coin.name}
+          symbol={coin.symbol}
+          price_usd={coin.price_usd}
+          percent_change_24h={coin.percent_change_24h}
+          percent_change_7d={coin.percent_change_7d}
+        />
+      );
+    });
+  };
+
   render() {
+    const { crypto } = this.props;
+
+    if (crypto.isFetching) {
+      return (
+        <View contentContainerStyle={ styles.contentContainer }>
+          <Spinner
+            visible={crypto.isFetching}
+            textContent={'Loading...'}
+            textStyle={{ color: '#253145' }}
+            animation='fade'
+          />
+        </View>
+      )
+    }
+
     return (
-      <View>
-        <Text>Container</Text>
-      </View>
+      <ScrollView>
+        {this.renderCoinCards()}
+      </ScrollView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  contentContainer: {
+    paddingBottom: 100,
+    paddingTop: 55,
+  },
+});
 
 const mapStateToProps = (state) => {
   const { crypto } = state;
